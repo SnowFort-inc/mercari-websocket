f7a6b6a618ad19276bb000fe6032d94b100d5b66
tsukinose81 <tsukinose81@aol.com>
2024-06-25 09:29:09 +0900

    chore: Add WebSocket server implementation with room functionality

--- index.ts
+++ index.ts
@@ -1,17 +1,40 @@
-// WebSocketサーバーの実装
 import { WebSocketServer } from "ws";
 
-// WebSocketサーバーをポート8080で起動
 const wss = new WebSocketServer({ port: 3000 });
+
+const rooms = new Map();
 
 wss.on("connection", function connection(ws) {
   console.log("クライアントが接続しました");
 
-  // クライアントからのメッセージを受信した時の処理
   ws.on("message", function message(data) {
-    console.log("受信したメッセージ:", data.toString());
+    const message = JSON.parse(data.toString());
+
+    if (message.type === "join") {
+      const roomName = message.room;
+      if (!rooms.has(roomName)) {
+        rooms.set(roomName, new Set());
+      }
+      rooms.get(roomName).add(ws);
+      console.log(`クライアントがルーム${roomName}に参加しました`);
+    } else if (message.type === "broadcast") {
+      const roomName = message.room;
+      if (rooms.has(roomName)) {
+        rooms.get(roomName).forEach((client: any) => {
+          if (client !== ws && client.readyState === WebSocket.OPEN) {
+            client.send(JSON.stringify(message));
+          }
+        });
+      }
+    }
   });
 
-  // クライアントへメッセージを送信
-  ws.send("サーバーからのメッセージ");
+  ws.on("close", () => {
+    rooms.forEach((clients, roomName) => {
+      if (clients.has(ws)) {
+        clients.delete(ws);
+        console.log(`クライアントがルーム${roomName}から退出しました`);
+      }
+    });
+  });
 });
